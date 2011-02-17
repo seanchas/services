b509ccf4f2d860fd303653de47d70fa925019b57
seanchas <seanchas@gmail.com>
2011-02-17 11:53:22 +0300

    prices aligned to left

--- config/initializers/infosell.rb
+++ config/initializers/infosell.rb
@@ -1,7 +1,7 @@
 Infosell.configure do |infosell|
   
   infosell.type = :info
-  infosell.site = "http://dls8:8092/"
+  infosell.site = "http://blis1:8092/"
   
   infosell.cache = {
     :service => 1.hour
