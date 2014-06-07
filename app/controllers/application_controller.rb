bf084a450abb345fdbf3d8c93742b6cacda0a789
Denis Aleshin <snorik.net@gmail.com>
2014-06-07 17:24:50 +0400

    modify clear_cache_control filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,7 +52,10 @@ protected
   end
 
   def clear_cache_control
-      response.headers.delete "Cache-Control"
+    # response.headers.delete "Cache-Control"
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
   
 end
